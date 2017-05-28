e5b21d78ddd2e7d705761d0ac11e02a6f6ca45d3
Enrico B <rflnrclws@protonmail.com>
2017-05-28 16:23:30 -0400

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Notebook from './components/Notebook';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/normalize.css';
 import './styles/index.css';
 
 ReactDOM.render(<Notebook />, document.getElementById('root'));
