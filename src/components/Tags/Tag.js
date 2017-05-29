9c956ad04dd1aa9f7c29680e2c1b5381ca74da20
Enrico B <rflnrclws@protonmail.com>
2017-05-29 13:21:16 -0400

    Wrap list of tags in div

--- src/components/Tags/Tag.js
+++ src/components/Tags/Tag.js
@@ -7,15 +7,13 @@ const Tag = (props) => {
   const isCurrentTag = props.name === props.currentTag;
   const tagClassName = isCurrentTag ? 'selected-tag' : 'tag-item';
   return (
-    <div
-      className={tagClassName}
-      onClick={() => props.updateCurrentTag(props.name)}
-    >
+    <div className={tagClassName}>
       <a>
         <input
           className="tag-input"
           type="text"
           value={`${props.name} (${props.count})`}
+          onClick={() => props.updateCurrentTag(props.name)}
           disabled
         />
       </a>
