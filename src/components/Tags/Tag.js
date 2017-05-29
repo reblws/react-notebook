9a7a470c602eb7bc255838ce92f2a60e08a8d30a
Enrico B <rflnrclws@protonmail.com>
2017-05-29 13:28:04 -0400

    Fix Tag not changing on a click

--- src/components/Tags/Tag.js
+++ src/components/Tags/Tag.js
@@ -7,13 +7,15 @@ const Tag = (props) => {
   const isCurrentTag = props.name === props.currentTag;
   const tagClassName = isCurrentTag ? 'selected-tag' : 'tag-item';
   return (
-    <div className={tagClassName}>
+    <div
+      className={tagClassName}
+      onClick={() => props.updateCurrentTag(props.name)}
+    >
       <a>
         <input
           className="tag-input"
           type="text"
           value={`${props.name} (${props.count})`}
-          onClick={() => props.updateCurrentTag(props.name)}
           disabled
         />
       </a>
