a7458529607dfecab0b5035dea579716d57a17ea
Enrico B <rflnrclws@protonmail.com>
2017-05-30 15:21:06 -0400

    Lint

--- src/components/Notes/NoteHeader.js
+++ src/components/Notes/NoteHeader.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import TagEditor from '../Tags/TagEditor';
 
-const NoteHeader = (props) => {
-  return (
-    <div className="note-header">
-      <input
-        type="text"
-        className="note-title"
-        value={props.title}
-        onChange={props.updateNoteTitle}
-      />
-      <TagEditor
-        id={props.id}
-        tags={props.tags}
-        updateNoteTags={props.updateNoteTags}
-      />
-    </div>
-  );
-};
+const NoteHeader = props => (
+  <div className="note-header">
+    <input
+      type="text"
+      className="note-title"
+      value={props.title}
+      onChange={props.updateNoteTitle}
+    />
+    <TagEditor
+      id={props.id}
+      tags={props.tags}
+      updateNoteTags={props.updateNoteTags}
+    />
+  </div>
+);
 
 
 export default NoteHeader;
