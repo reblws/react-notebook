670737f6d823d34a79a181a9658562984c204ef4
Enrico B <rflnrclws@protonmail.com>
2017-05-29 21:48:53 -0400

    Add TagEditor for manipulating note tags

---
+++ src/components/Tags/TagEditor.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class TagEditor extends React.Component {
+
+  static joinTagArray(tagArray) {
+    return tagArray
+      .map(tag => `#${tag}`)
+      .join(' ');
+  }
+
+  static parseTagString(tagString) {
+    const hashtagPattern = /\s*?#/g;
+    return tagString.split(hashtagPattern);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tagString: TagEditor.joinTagArray(this.props.tags),
+    };
+    this.handleTagChange = this.handleTagChange.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.id === this.props.id) return;
+    this.setState({
+      tagString: TagEditor.joinTagArray(nextProps.tags),
+    });
+  }
+
+
+  handleTagChange(event) {
+    // Handle updating local store somehow
+    //this.props.updateNoteTags
+    this.setState({
+      tagString: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="note-tags"
+        value={this.state.tagString}
+        onChange={this.handleTagChange}
+      />
+    );
+  }
+}
+
+export default TagEditor;
